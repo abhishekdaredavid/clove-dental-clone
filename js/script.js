09fbc2040bf4e68befedbfea9424f835903cde15
abhishekdaredavid <146186138+abhishekdaredavid@users.noreply.github.com>
2025-06-18 20:52:47 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-function toggleFAQ(el) {
-  const p = el.nextElementSibling;
-  p.style.display = (p.style.display === "block") ? "none" : "block";
-}
-function submitForm(e) {
-  e.preventDefault();
-  alert("Thank you! Your appointment request has been submitted.");
-}
