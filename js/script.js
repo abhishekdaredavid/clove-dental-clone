3e75081a5b1eb8141b84f41db0ec899b9f9b2d67
abhishekdaredavid <146186138+abhishekdaredavid@users.noreply.github.com>
2025-06-18 20:26:57 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-function toggleFAQ(element) {
-  const next = element.nextElementSibling;
-  next.style.display = next.style.display === 'block' ? 'none' : 'block';
-}
-function submitForm(event) {
-  event.preventDefault();
-  alert('Thank you! We will contact you soon.');
-}
